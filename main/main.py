8ab90725bb4341e50aa53be4c87854faa3d6b50a
William Doyle <williamdoyleiii@gmail.com>
2017-11-08 11:49:27 -0500

    added articleFile printing

--- main/main.py
+++ main/main.py
@@ -5,16 +5,22 @@ from trec_car.read_data import *
 
 def run():
     print("Running IR-Ext...")
-    if len(sys.argv) < 1:
-        print("usage ", sys.argv[0], " paragraphFile")
+    if len(sys.argv) < 4:
+        print("usage ", sys.argv[0], " articleFile outlineFile paragraphFile")
     classify.prelim.do_classification()
     with open(sys.argv[1], 'rb') as f:
-        for p in iter_paragraphs(f):
-            print('\n', p.para_id, ':')
-            texts = [elem.text if isinstance(elem, ParaText)
-                     else elem.anchor_text
-                     for elem in p.bodies]
-            print(' '.join(texts))
+        for p in iter_annotations(f):
+            print('\npagename: ', p.page_name)
+            print('\npageid: ', p.page_id)
+            print('\nmeta: ', p.page_meta)
+
+    # with open(sys.argv[3], 'rb') as f:
+    #     for p in iter_paragraphs(f):
+    #         print('\n', p.para_id, ':')
+    #         texts = [elem.text if isinstance(elem, ParaText)
+    #                  else elem.anchor_text
+    #                  for elem in p.bodies]
+    #         print(' '.join(texts))
 
 
 if __name__ == "__main__":
