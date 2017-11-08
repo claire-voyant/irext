1132b44539fd89650b6387fb53519a52074ecd7d
William Doyle <williamdoyleiii@gmail.com>
2017-11-08 11:53:22 -0500

    able to print all of the trec data

--- main/main.py
+++ main/main.py
@@ -3,8 +3,7 @@ import sys
 from trec_car.read_data import *
 
 
-def run():
-    print("Running IR-Ext...")
+def print_data():
     if len(sys.argv) < 4:
         print("usage ", sys.argv[0], " articleFile outlineFile paragraphFile")
     classify.prelim.do_classification()
@@ -14,14 +13,21 @@ def run():
             print('\npageid: ', p.page_id)
             print('\nmeta: ', p.page_meta)
 
-    # with open(sys.argv[3], 'rb') as f:
-    #     for p in iter_paragraphs(f):
-    #         print('\n', p.para_id, ':')
-    #         texts = [elem.text if isinstance(elem, ParaText)
-    #                  else elem.anchor_text
-    #                  for elem in p.bodies]
-    #         print(' '.join(texts))
+    with open(sys.argv[2], 'rb') as f:
+        for p in iter_annotations(f):
+            print('\npagename:', p.page_name)
+            for heading in p.outline():
+                print(heading)
+
+    with open(sys.argv[3], 'rb') as f:
+        for p in iter_paragraphs(f):
+            print('\n', p.para_id, ':')
+            texts = [elem.text if isinstance(elem, ParaText)
+                     else elem.anchor_text
+                     for elem in p.bodies]
+            print(' '.join(texts))
 
 
 if __name__ == "__main__":
-    run()
+    print("Running IR-Ext...")
+    print_data()
