5de4cf5730a5625b4273f4f4faa642fa47709d55
William Doyle <williamdoyleiii@gmail.com>
2017-12-01 09:24:20 -0500

    updated setup

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     license='',
     author='team7',
     author_email='',
-    description='Information Retrieval Tool for Classification', requires=['cbor']
+    description='Information Retrieval Tool for Classification', requires=['cbor', 'sklearn', 'IPython', 'pandas', 'numpy', 'trec_car']
 )
